c7a66222d1b74e86c850936b923e050b9481d78a
Chris Scutt <chris.scutt@brainflash.uk>
2020-11-24 12:20:57 +0000

    Initial commit

---
+++ Chargey/ChargeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChargeyApp.swift
+//  Chargey
+//
+//  Created by Brainflash on 24/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ChargeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
